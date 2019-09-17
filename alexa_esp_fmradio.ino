20195dd485fd6b102cbcb53e986ca27e2abd1642
yus <yus.projectproto@gmail.com>
2019-09-17 16:31:53 +0800

    connect to wifi network

--- alexa_esp_fmradio.ino
+++ alexa_esp_fmradio.ino
@@ -1,14 +1,41 @@
 #include <ESP8266WiFi.h>
 
+
 #define SERIAL_BAUDRATE                 115200
 #define LED                             0
 
+
+/* WiFi */
+#include "credentials.h" // define ssid and password here
+void wifiSetup() {
+  // set module to station mode
+  WiFi.mode(WIFI_STA);
+
+  // connect
+  Serial.printf("[WIFI] Connecting to %s ", WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  // wait
+  while (WL_CONNECTED != WiFi.status()) {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println();
+
+  // connected
+  Serial.printf("[WIFI] STA Mode, SSID: %s, IP address: %s\n", WiFi.SSID().c_str(), WiFi.localIP().toString().c_str());
+}
+
 void setup() {
-  // Init debug serial port
+  // init debug serial port
   Serial.begin(SERIAL_BAUDRATE);
   Serial.println();
 
+  // LED
   pinMode(LED, OUTPUT);
+
+  // WiFi
+  wifiSetup();
 
 }
 
