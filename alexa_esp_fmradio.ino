d5e64c1d9ccc841294cb891e95551857ddeadc82
yus <yus.projectproto@gmail.com>
2019-09-17 17:43:49 +0800

    setup fauxmo virtual device

--- alexa_esp_fmradio.ino
+++ alexa_esp_fmradio.ino
@@ -1,9 +1,15 @@
 #include <ESP8266WiFi.h>
 #include <ESPAsyncWebServer.h>
+#include <fauxmoESP.h>
 
 
-#define SERIAL_BAUDRATE                 115200
-#define LED                             0
+#define VIRTUAL_DEVICE_NAME             "fm-radio"
+#define WEBSERVER_PORT                  (80)
+#define SERIAL_BAUDRATE                 (115200)
+#define LED_PIN                         (0)
+
+
+fauxmoESP fauxmo;
 
 
 /* WiFi */
@@ -28,14 +34,27 @@ void wifiSetup() {
 }
 
 /* web server */
-AsyncWebServer server(80);
+AsyncWebServer server(WEBSERVER_PORT);
 void serverSetup() {
   // custom entry point
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(200, "text/plain", "Hello, world");
   });
 
+  // callbacks
+  server.onRequestBody([](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
+    if (fauxmo.process(request->client(), request->method() == HTTP_GET, request->url(), String((char *)data))) {
+      return;
+    }
+    //
+  });
+
+  // 404
   server.onNotFound([](AsyncWebServerRequest *request) {
+    String body = (request->hasParam("body", true)) ? request->getParam("body", true)->value() : String();
+    if (fauxmo.process(request->client(), request->method() == HTTP_GET, request->url(), body)) {
+      return;
+    }
     request->send(404, "text/plain", "Not found");
   });
 
@@ -49,7 +68,7 @@ void setup() {
   Serial.println();
 
   // LED
-  pinMode(LED, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
 
   // WiFi
   wifiSetup();
@@ -57,12 +76,30 @@ void setup() {
   // web server
   serverSetup();
 
+  // fauxmoESP settings
+  fauxmo.createServer(false); // custom tcp server
+  fauxmo.setPort(WEBSERVER_PORT);
+  fauxmo.enable(true);
+
+  // add virtual devices
+  fauxmo.addDevice(VIRTUAL_DEVICE_NAME);
+
+  fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value) {
+    Serial.printf("[MAIN] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
+    digitalWrite(LED_PIN, !state);
+  });
+
 }
 
 void loop() {
-  digitalWrite(LED, HIGH);
-  delay(500);
-  digitalWrite(LED, LOW);
-  delay(500);
+  // poll udp packets
+  fauxmo.handle();
+
+  // debug free heap
+  static unsigned long last_ms = millis();
+  if (millis() - last_ms > 5000) {
+    last_ms = millis();
+    Serial.printf("[MAIN] Free heap: %d bytes\n", ESP.getFreeHeap());
+  }
 
 }
